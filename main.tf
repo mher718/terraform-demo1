2050690c6048c316cbbf89220b4aaf70ae697a00
Elmer Amigleo <elmer.amigleo@idemia.com>
2025-11-18 15:27:56 -0500

    fix main.tf provisioner part

--- main.tf
+++ main.tf
@@ -49,7 +49,7 @@ resource "vsphere_virtual_machine" "vm" {
   // Provisioning
   provisioner "file" {
     source      = "scripts/setup.sh"
-    destination = "setup.sh"
+    destination = "/tmp/setup.sh"
   }
 
   //provisioner "file" {
@@ -59,8 +59,8 @@ resource "vsphere_virtual_machine" "vm" {
   provisioner "remote-exec" {
     inline = [
       "sleep 30",
-      "sudo chmod +x setup.sh",
-      "sudo bash setup.sh"
+      "sudo chmod +x /tmp/setup.sh",
+      "sudo bash /tmp/setup.sh"
     ]
   }
   //provisioner_user        = "administrator"
